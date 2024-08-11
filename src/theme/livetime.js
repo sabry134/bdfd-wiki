a8054fc619bf580f0a1ead4c0f5ef3fe1c4a3337
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2024-08-11 14:02:11 +0300

    Live Time Fix

--- src/theme/livetime.js
+++ src/theme/livetime.js
@@ -1,11 +1,32 @@
-var day = new Date().getDate();
-var time = new Date().getTime();
-var second = new Date().getSeconds();
-var minute = new Date().getMinutes();
-var hour = new Date().getHours();
+document.addEventListener("DOMContentLoaded", function() {
+    var day = new Date().getDate();
+    var time = new Date().getTime();
+    var second = new Date().getSeconds();
+    var minute = new Date().getMinutes();
+    var hour = new Date().getHours();
 
-document.getElementById('day').innerHTML = "Current Day: " + day;
-document.getElementById('unix').innerHTML = time;
-document.getElementById('second').innerHTML = "Current Second: " + second;
-document.getElementById('minute').innerHTML = "Current Minute: " + minute;
-document.getElementById('hour').innerHTML = "Current Hour: " + hour;
+    var dayElement = document.getElementById('day-mark');
+    if (dayElement) {
+        dayElement.innerHTML = "Current Day: " + day;
+    }
+
+    var unixElement = document.getElementById('unix-mark');
+    if (unixElement) {
+        unixElement.innerHTML = "Current Unix-time: " + time;
+    }
+
+    var secondElement = document.getElementById('second-mark');
+    if (secondElement) {
+        secondElement.innerHTML = "Current Second: " + second;
+    }
+
+    var minuteElement = document.getElementById('minute-mark');
+    if (minuteElement) {
+        minuteElement.innerHTML = "Current Minute: " + minute;
+    }
+
+    var hourElement = document.getElementById('hour-mark');
+    if (hourElement) {
+        hourElement.innerHTML = "Current Hour: " + hour;
+    }
+});
